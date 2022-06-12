4848b02f90cd85e3fde1fe90baeec17a2d335007
MrSpock1994 <willpokerpro2016@gmail.com>
2022-06-11 22:33:35 -0300

    first commit

--- books2.py
+++ books2.py
@@ -1,82 +1,23 @@
-from fastapi import FastAPI
+from urllib.request import Request
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 from uuid import UUID
 from typing import Optional
+from starlette.responses import JSONResponse
+
+
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
+
+
 
 app = FastAPI()
 
 
-class Book(BaseModel):
-    id: UUID
-    title: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=100)
-    description: Optional[str] = Field(min_length=1, max_length=100, 
-                        title="Description of the book")
-    rating: int = Field(gt=-1, lt=101)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": "af2db729-ae20-425c-a55f-d67ed1658ec7",
-                "title": "Computer science",
-                "author": "Coding",
-                "Description": "Nothing",
-                "rating": 75
-            }
-        }
-
-
-BOOKS = []
-
-
-
-@app.get("/")
-async def read_all_books(books_to_return: Optional[int] = None):
-    if len(BOOKS) < 1:
-        create_books_no_api()
-    
-    if books_to_return and len(BOOKS) >= books_to_return > 0:
-        i = 1
-        new_books =[]
-        while i <= books_to_return:
-            new_books.append(BOOKS[i - 1])
-            i += 1
-        return new_books
-    return BOOKS
-
-
-@app.get("/book/{book_id}")
-async def read_book(book_id: UUID):
-    for x in BOOKS:
-        if x.id == book_id:
-            return x
-
-
-
-@app.post("/")
-async def create(book: Book):
-    BOOKS.append(book)
-    return book
-
-
-@app.put("/{book_id}")
-async def update_book(book_id: UUID, book: Book):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS[counter - 1] = book
-            return BOOKS[counter - 1]
-
-
-@app.delete("/{book_id}")
-async def delete_book(book_id: UUID):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS.pop(counter - 1)
-            return BOOKS
+def raise_item_cannot_be_found():
+    return HTTPException(status_code=404, detail="Book not found!",  headers={"X-Header-Error":"Nothing to be seen at the UUID"})
 
 
 def create_books_no_api():
@@ -104,3 +45,92 @@ def create_books_no_api():
     BOOKS.append(book_2)
     BOOKS.append(book_3)
     BOOKS.append(book_4)    
+
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(min_length=1, max_length=100, 
+                        title="Description of the book")
+    rating: int = Field(gt=-1, lt=101)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "af2db729-ae20-425c-a55f-d67ed1658ec7",
+                "title": "Computer science",
+                "author": "Coding",
+                "Description": "Nothing",
+                "rating": 75
+            }
+        }
+
+
+BOOKS = []
+
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request, exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={"Message":"Hey, why do you want {exception.books_to_return}?"}
+    )
+
+
+@app.get("/")
+async def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+    if len(BOOKS) < 1:
+        create_books_no_api()
+    if books_to_return and len(BOOKS) >= books_to_return > 0:
+        i = 1
+        new_books =[]
+        while i <= books_to_return:
+            new_books.append(BOOKS[i - 1])
+            i += 1
+        return new_books
+    return BOOKS
+
+
+
+
+@app.get("/book/{book_id}")
+async def read_book(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    raise raise_item_cannot_be_found()
+
+
+@app.post("/")
+async def create(book: Book):
+    BOOKS.append(book)
+    return book
+
+
+@app.put("/{book_id}")
+async def update_book(book_id: UUID, book: Book):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS[counter - 1] = book
+            return BOOKS[counter - 1]
+raise raise_item_cannot_be_found()
+
+
+@app.delete("/{book_id}")
+async def delete_book(book_id: UUID):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS.pop(counter - 1)
+            return BOOKS
+    raise raise_item_cannot_be_found()
+
+
+
+
